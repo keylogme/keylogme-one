3ba3bbb8c5fa2696a378dadc87764ddc91f87135
esoteloferry <efrain.sotelo.ferry@gmail.com>
2025-06-21 21:21:47 +0200

    fix(config): get config depends on OS

--- cmd/main.go
+++ cmd/main.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"runtime"
 	"time"
 
 	k0 "github.com/keylogme/keylogme-zero"
@@ -30,11 +31,13 @@ func main() {
 	// Get setup
 	APIKEY := os.Getenv("KEYLOGME_ONE_API_KEY")
 	if APIKEY == "" {
-		log.Fatal("API_KEY env var is not set")
+		log.Fatal("KEYLOGME_ONE_API_KEY env var is not set")
 	}
 	//****************************************************
 
-	res, err := http.Get(fmt.Sprintf("%s/config?apikey=%s", KEYLOGME_ENDPOINT, APIKEY))
+	res, err := http.Get(
+		fmt.Sprintf("%s/config?apikey=%s&os=%s", KEYLOGME_ENDPOINT, APIKEY, runtime.GOOS),
+	)
 	if err != nil {
 		log.Fatal(err)
 	}
