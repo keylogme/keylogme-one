3b8dfe77118a8039ce00fbac4e4c48b60bac9dbe
esoteloferry <efrain.sotelo.ferry@gmail.com>
2025-06-21 22:34:06 +0200

    fix(deploy): trigger

--- internal/sender.go
+++ internal/sender.go
@@ -308,7 +308,7 @@ func (s *Sender) read() {
 
 func (s *Sender) Send(p []byte) error {
 	if len(s.writer) == maxQueueSizeWrite {
-		slog.Info("Sender queue is full -- Sender is closed")
+		slog.Info("Sender queue is full")
 	}
 	if s.closed {
 		slog.Info(
